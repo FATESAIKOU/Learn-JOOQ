83262cb0f71fd561e5ab807144c0cc796897a720
FATESAIKOU <qzsecftbhhhh@gmail.com>
2025-05-04 18:23:45 +0900

    Rearrange2

--- src/main/java/com/fatesaikou/test/jooq/learn/JooqtestApplication.java
+++ src/main/java/com/fatesaikou/test/jooq/learn/JooqtestApplication.java
@@ -3,13 +3,7 @@ package com.fatesaikou.test.jooq.learn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.TypeHint;
 
-@NativeHint(
-    options = "--enable-url-protocols=http,https",
-    types = @TypeHint(types = org.postgresql.Driver.class)
-)
 @SpringBootApplication
 @MapperScan("com.fatesaikou.test.jooq.learn.mapper")
 public class JooqtestApplication {
